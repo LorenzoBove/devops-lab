b8cf719aa7fd148c7b39f53326c5a7f50aa9554b
Lorenzo Bove <lorenzobove@Lorenzos-MacBook-Pro.local>
2026-07-02 09:41:17 +0200

    added first main with basic app functions

---
+++ app/main.py
@@ -0,0 +1,15 @@
+
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "DevOps Lab API"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
