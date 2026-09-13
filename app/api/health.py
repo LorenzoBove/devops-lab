70eba3b8209be1457aba812c087c971b6ed4633f
Lorenzo Bove <lorenzobove@Mac-mini-di-Lorenzo.local>
2026-09-13 19:21:33 +0200

    reverse to normal deployment process

--- app/api/health.py
+++ app/api/health.py
@@ -1,19 +1,4 @@
 
-
-"""
-from fastapi import APIRouter, HTTPException
-
-
-router = APIRouter()
-
-@router.get("/health")
-def health():
-    return {"status" : "ok"}
-
-"""
-
-
-
 import os
 
 from fastapi import APIRouter, HTTPException
