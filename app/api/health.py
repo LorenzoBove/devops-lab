a31501652c0b74f26e6ebd3cb1e4714601dc7e53
Lorenzo Bove <lorenzobove@Mac-mini-di-Lorenzo.local>
2026-09-13 19:11:15 +0200

    Test rollback through deploy script

--- app/api/health.py
+++ app/api/health.py
@@ -1,6 +1,6 @@
 
 
-
+"""
 from fastapi import APIRouter, HTTPException
 
 
@@ -13,15 +13,29 @@ def health():
 """
 
 
+
+import os
+
+from fastapi import APIRouter, HTTPException
+
+
 router = APIRouter()
+
 
 @router.get("/health")
 def health():
-    raise HTTPException(
-        status_code=500,
-        detail="Intentional health check failure"
-    )
 
+    force_health_fail = os.getenv(
+        "FORCE_HEALTH_FAIL",
+        "false"
+    ).lower() == "true"
 
+    if force_health_fail:
+        raise HTTPException(
+            status_code=500,
+            detail="Intentional health check failure"
+        )
 
-"""
+    return {
+        "status": "ok"
+    }
